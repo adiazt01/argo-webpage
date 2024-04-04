e38bc210f3bfc07b2dfda07a8741a6556b3729d7
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-04-04 12:18:51 -0400

    Fix import statement in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { LandingPage } from "./pages/LandingPage";
 import { BlogProvider } from "./context/BlogContext";
 import { BlogPage } from "./pages/BlogPage";
-import "./style/animations/AnimationScroll.css"
+import "./style/animations/AnimationScroll.css";
 
 const rootElement = document.getElementById("root");
 
@@ -13,21 +13,26 @@ if (!rootElement) {
   throw new Error("Could not find root element");
 }
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/blog",
+      element: <BlogPage />,
+    },
+  ],
   {
-    path: "/",
-    element: <LandingPage />,
+    basename: "/argo-webpage",
   },
-  {
-    path: "/blog",
-    element: <BlogPage />,
-  },
-]);
+);
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <BlogProvider>
-      <RouterProvider router={router}  />
+      <RouterProvider router={router} />
     </BlogProvider>
   </React.StrictMode>,
 );
