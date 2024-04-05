9160e43d5b6344bd4c8b1bfb0a00c3519de37769
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-04-04 20:18:35 -0400

    Add firstVisit state and update changePage function

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,8 +1,9 @@
-import { useMemo, useState, useEffect, useRef } from "react";
+import { useMemo, useState, useEffect, useRef, useCallback } from "react";
 import { Post } from "../types/Blog";
 
 export const usePagination = (data: Post[], itemsPerPage: number) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [firstVisit, setFirstVisit] = useState(true); // New state
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
@@ -14,18 +15,18 @@ export const usePagination = (data: Post[], itemsPerPage: number) => {
   const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
   const sectionRef = useRef<HTMLElement>(null);
 
-  useEffect(() => {
-    if (sectionRef.current) {
-      sectionRef.current.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  }, [currentPage]);
-
-  const changePage = (index: number) => {
+  const changePage = useCallback((index: number) => {
     setCurrentPage(index + 1);
-  };
+    if (firstVisit) setFirstVisit(false); // Set firstVisit to false after first page change
+  }, [firstVisit]); // Add firstVisit to dependencies
+
+  useEffect(() => {
+    if (!sectionRef.current || firstVisit) return; // Check if firstVisit is true
+    window.scrollTo({
+      top: sectionRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  }, [currentPage, firstVisit]); // Add firstVisit to the dependencies
 
   return {
     currentItems,
--- src/sections/blog/HeroSection.tsx
+++ src/sections/blog/HeroSection.tsx
@@ -1,4 +1,4 @@
-import {  useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { BlogContext } from "../../context/BlogContext";
 import { BlogCard } from "../../components/cards/BlogCard";
 import { LayoutSection } from "../../layout/LayoutSection";
@@ -7,6 +7,8 @@ import { adaptPost } from "../../helpers/postUtils";
 import { usePagination } from "../../hooks/usePagination";
 import { ITEMS_PER_PAGE } from "../../constants";
 import { Pagination } from "../../components/pagination/Pagination";
+import { womenEnterprise } from "../../assets";
+import { Typography } from "../../components/Typography";
 
 export const HeroSection = () => {
   const [selectedTag, setSelectedTag] = useState("");
@@ -18,11 +20,18 @@ export const HeroSection = () => {
 
   const posts = useMemo(() => postsData.results.map(adaptPost), [postsData]);
 
-  const tags = useMemo(() => [...new Set(posts.flatMap((post) => post.tags))], [posts]);
+  const tags = useMemo(
+    () => [...new Set(posts.flatMap((post) => post.tags))],
+    [posts],
+  );
 
-  const filteredPosts = useMemo(() => selectedTag
-    ? posts.filter((post) => post.tags.includes(selectedTag))
-    : posts, [selectedTag, posts]);
+  const filteredPosts = useMemo(
+    () =>
+      selectedTag
+        ? posts.filter((post) => post.tags.includes(selectedTag))
+        : posts,
+    [selectedTag, posts],
+  );
 
   const {
     changePage: originalChangePage,
@@ -36,14 +45,23 @@ export const HeroSection = () => {
 
   return (
     <>
-      <LayoutSection id="hero-section">
-        <header className="flex min-h-screen w-full border border-red-500">
-          <div className="flex min-h-screen w-full flex-col items-center justify-center text-black">
-            <h1 className="text-4xl font-bold">Blog</h1>
-            <p className="text-lg">Welcome to the blog</p>
+      <main className="mx-auto my-24 flex max-w-6xl flex-col justify-start gap-12 border border-red-500 px-4">
+        <header className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div className="flex flex-col items-center justify-center rounded-md bg-gradient-to-br from-blue-900 to-blue-700 p-4">
+            <Typography variant="h1">Nuestro blog</Typography>
+            <Typography variant="p">
+              Entérate de las últimas noticias y tendencias en el mundo de la
+              tecnología y los negocios.
+            </Typography>
           </div>
+          <picture className="flex flex-col ">
+            <img
+              src={womenEnterprise}
+              className="h-full w-full rounded-md object-cover"
+            />
+          </picture>
         </header>
-      </LayoutSection>
+      </main>
 
       <LayoutSection ref={sectionRef} id="blog-posts">
         <div>
